112c6c48fb4bd1cd576ab373fe48cc8c430fed22
weDevs <38912435+anis3139@users.noreply.github.com>
2021-12-30 21:56:37 +0600

    Class 6

---
+++ images2/Website Layout Design on Behance_files/network.41.fd04f2c9f5b3e2ec486a.js
@@ -0,0 +1,2 @@
+(("undefined"!=typeof global?global:self).webpackJsonp_webpack_network_bundle=("undefined"!=typeof global?global:self).webpackJsonp_webpack_network_bundle||[]).push([[41],{zfCw:function(e,_,a){e.exports=function(){"use strict";return{name:"en",weekdays:"Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday".split("_"),months:"January_February_March_April_May_June_July_August_September_October_November_December".split("_")}}()}}]);
+//# sourceMappingURL=network.41.fd04f2c9f5b3e2ec486a.js.map
