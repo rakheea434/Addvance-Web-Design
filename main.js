0c1e6bd6348a48874dc67ac9220e5c134ac97d4c
weDevs <38912435+anis3139@users.noreply.github.com>
2022-02-27 21:41:44 +0600

    Class 28

--- main.js
+++ main.js
@@ -1,43 +1,12 @@
-/**
- * CRUD
- *  C for Create
- *  R for Read
- *  U for update
- *  D for Delete
- */ 
- 
- const user="admin";
- const password="12345"
- 
-let btn =document.getElementById('btn')
-let userName =document.getElementById('userName');
-let pass =document.getElementById('pass');  
-let error =document.getElementById('error');  
+let body= $('body')
+let btn= $('#btn')
 
-btn.addEventListener('click', function(event){
-    event.preventDefault() ;
-    error.style.display='block';
-    if(userName.value.length==0){
-        error.innerHTML='User name is Empty';
-    }else if(pass.value.length==0){
-       
-        error.innerHTML='Password is Empty';
-    }else{
-
-        if(userName.value === user && pass.value === password){
-            error.style.color='blue'
-            error.innerHTML='You are loged in';
-        } else if(userName.value !== user){
-             error.innerHTML='User name is wrong'; 
-       } else if(pass.value !== password){
-            error.innerHTML='Password is wrong';   
-       }
-       else{
-            error.innerHTML='Something is wrong';  
-       }
-    }
-    
-    
+btn.click(function(e){
+    e.preventDefault()
+    var red= Math.round(Math.random()*255);
+    var green= Math.round(Math.random()*255);
+    var blue= Math.round(Math.random()*255);  
+   body.css('background-color', `rgb(${red}, ${green}, ${blue})`)
 })
 
-
+ 
